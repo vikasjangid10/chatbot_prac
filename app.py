06c20bec3cd68653773edb7084e214404d30de14
vikasjangid10 <v7776139@gmail.com>
2025-01-20 12:51:30 +0530

    first

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+import google.generativeai as genai
+
+import os
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content(question)
+    return response.text
+
+st.set_page_config(page_title='Chatbot for Q%A')
+st.header('Chatbot for Q@A by vikas')
+
+input = st.text_input("Input :" , key="input")
+
+submit = st.button("Get Response!")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader("the response is :")
+    st.write(response)
